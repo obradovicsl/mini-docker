1c957cab97e31d258f1e44311de497b17f1271f2
obradovicsl <slobodanobradovic3@gmail.com>
2025-06-06 15:09:09 +0200

    Stoud & stderr piped

--- app/main.go
+++ app/main.go
@@ -12,25 +12,34 @@ var _ = exec.Command
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-	// You can use print statements as follows for debugging, 
+	// You can use print statements as follows for debugging,
 	// they'll be visible when running tests.
-
 
 	// fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage!
-	
+	// mydocker run alpine:latest /usr/local/bin/docker-explorer echo hey
+
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
-	print(command, args)
-	
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
-	if err != nil {
-		fmt.Printf("Err: %v", err)
-		os.Exit(1)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Fatall: ", err)
 	}
-	
-	fmt.Println(string(output))
+
+	// output, err := cmd.Output()
+	// if err != nil {
+	// 	fmt.Printf("Err: %v", err)
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Println(string(output))
 }
+
+// go build -o applicaion app/*.go
+// application run <image> <command> <arg1> <arg2>
+
+// your_docker.sh run <image> <command> <arg1> <arg2>
